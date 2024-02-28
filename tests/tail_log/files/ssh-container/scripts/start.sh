9185aa4ebe38c965076835b03a853a70f5572853
declercklouis <declerck.louis@outlook.com>
2024-02-28 08:19:51 +0100

    updates

---
+++ tests/tail_log/files/ssh-container/scripts/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/sbin/sshd -D # Start the SSH server and run it in the foreground
---
+++ tests/ubuntu/files/scripts/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/usr/sbin/sshd -D # Start the SSH server and run it in the foreground
+
+```
